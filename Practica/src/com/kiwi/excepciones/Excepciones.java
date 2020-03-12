9d4a8671b56da19ee7094ba688bd34596b46d703
hugoskl <hugosklx3@gmail.com>
2020-03-12 19:02:30 +0100

    Nuevas excepciones

--- Practica/src/com/kiwi/excepciones/Excepciones.java
+++ Practica/src/com/kiwi/excepciones/Excepciones.java
@@ -3,7 +3,11 @@ package com.kiwi.excepciones;
 public class Excepciones extends Exception{
     static String[] excepcion = new String[]{
             "+1",
-            "< ERROR 001: Username o Password incorrecto >",
+            "< ERROR 001: Username o Password incorrecto >\n",
+            "< ERROR 002: Las contraseñas no coinciden >\n",
+            "< ERROR 003: Opcion desconocida >\n",
+            "< ERROR 004: El username es igual que el original >\n",
+
 
     };
 
